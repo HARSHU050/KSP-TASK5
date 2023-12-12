33f76bfea66a3ec2cbb230693b776e8a59b3b933
CH HARSHITHA <127428882+HARSHU050@users.noreply.github.com>
2023-12-12 20:31:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// operators
+console.log("hello--- world");
+let a=10;
+let b=20;
+let h="20";
+// arithmetic operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+// assignment operators
+console.log(a+=b);
+console.log(a-=b);
+// same follows for *=,/=,%=,**= 
+//COMPARISON OPERATOR
+console.log(b==h);//"=="compares the value irrespective of data type
+console.log(b===h);//"==="compares the value  as well as the data type
+/*-----------*/
+let c="harshu";
+console.log("c");
+console.log("20");
+let no=10;
+no=30;
+console.log(no);
+var n=30;
+n=40;
+console.log(n);
+const number=100;
+console.log(number);
+//DATATYPES
+let number1=300;//integer
+let word="car";//string
+let box=false;//boolean
+console.log(box);
+
+
+
